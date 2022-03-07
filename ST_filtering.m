1640729439e37fd1a1d58063443265877acade12
Thodoris Tyrovouzis <tyrovouzis@aristurtle.gr>
2022-03-07 13:57:44 +0200

    minor filtering coefficients fix

--- ST_filtering.m
+++ ST_filtering.m
@@ -6,20 +6,20 @@ d = zeros(size(s));
 lars_interp = 0.75*PrevLars+0.25*lars;
 k = arrayfun(@lar_inv, lars_interp);
 a = rc2poly(k);
-d(1:13) = filter([1 -a], 1, s(1:13));
+d(1:13) = filter(a, 1, s(1:13));
 
 lars_interp = 0.5*PrevLars+0.5*lars;
 k = arrayfun(@lar_inv, lars_interp);
 a = rc2poly(k);
-d(14:27) = filter([1 -a], 1, s(14:27));
+d(14:27) = filter(a, 1, s(14:27));
 
 lars_interp = 0.25*PrevLars+0.75*lars;
 k = arrayfun(@lar_inv, lars_interp);
 a = rc2poly(k);
-d(28:40) = filter([1 -a], 1, s(28:40));
+d(28:40) = filter(a, 1, s(28:40));
 
 k = arrayfun(@lar_inv, lars);
 a = rc2poly(k);
-d(41:end) = filter([1 -a], 1, s(41:end));
+d(41:end) = filter(a, 1, s(41:end));
 end
 
