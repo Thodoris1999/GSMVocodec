a0a41107f695142d4e72a2627955a1c873179d44
Thodoris Tyrovouzis <tyrovouzis@aristurtle.gr>
2022-03-07 17:09:32 +0200

    Fix synthesis filter

--- ST_synthesis.m
+++ ST_synthesis.m
@@ -6,20 +6,20 @@ s = zeros(size(d_prime));
 lars_interp = 0.75*PrevLars+0.25*lars;
 k = arrayfun(@lar_inv, lars_interp);
 a = rc2poly(k);
-s(1:13) = filter(a, 1, d_prime(1:13));
+s(1:13) = filter(1, a, d_prime(1:13));
 
 lars_interp = 0.5*PrevLars+0.5*lars;
 k = arrayfun(@lar_inv, lars_interp);
 a = rc2poly(k);
-s(14:27) = filter(a, 1, d_prime(14:27));
+s(14:27) = filter(1, a, d_prime(14:27));
 
 lars_interp = 0.25*PrevLars+0.75*lars;
 k = arrayfun(@lar_inv, lars_interp);
 a = rc2poly(k);
-s(28:40) = filter(a, 1, d_prime(28:40));
+s(28:40) = filter(1, a, d_prime(28:40));
 
 k = arrayfun(@lar_inv, lars);
 a = rc2poly(k);
-s(41:end) = filter(a, 1, d_prime(41:end));
+s(41:end) = filter(1, a, d_prime(41:end));
 end
 
